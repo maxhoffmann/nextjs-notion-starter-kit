11be737370a2eee4266f6f53acf4bab60bd6c4f2
Maximilian Hoffmann <hey@max.hn>
2022-01-09 14:37:56 +0100

    remove unused styles and fathom

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -15,7 +15,7 @@ import 'prismjs/themes/prism-coy.css'
 // import 'rc-dropdown/assets/index.css'
 
 // used for rendering equations (optional)
-import 'katex/dist/katex.min.css'
+// import 'katex/dist/katex.min.css'
 
 // core styles for static tweet renderer (optional)
 import 'react-static-tweets/styles.css'
@@ -24,44 +24,44 @@ import 'react-static-tweets/styles.css'
 import 'styles/notion.css'
 
 // global style overrides for prism theme (optional)
-import 'styles/prism-theme.css'
+// import 'styles/prism-theme.css'
 
 // here we're bringing in any languages we want to support for
 // syntax highlighting via Notion's Code block
-import 'prismjs'
-import 'prismjs/components/prism-markup'
-import 'prismjs/components/prism-javascript'
-import 'prismjs/components/prism-typescript'
-import 'prismjs/components/prism-bash'
+// import 'prismjs'
+// import 'prismjs/components/prism-markup'
+// import 'prismjs/components/prism-javascript'
+// import 'prismjs/components/prism-typescript'
+// import 'prismjs/components/prism-bash'
 
 import React from 'react'
-import { useRouter } from 'next/router'
+// import { useRouter } from 'next/router'
 import { bootstrap } from 'lib/bootstrap-client'
-import { fathomId, fathomConfig } from 'lib/config'
-import * as Fathom from 'fathom-client'
+// import { fathomId, fathomConfig } from 'lib/config'
+// import * as Fathom from 'fathom-client'
 
 if (typeof window !== 'undefined') {
   bootstrap()
 }
 
 export default function App({ Component, pageProps }) {
-  const router = useRouter()
+  // const router = useRouter()
 
-  React.useEffect(() => {
-    if (fathomId) {
-      Fathom.load(fathomId, fathomConfig)
+  // React.useEffect(() => {
+  //   if (fathomId) {
+  //     Fathom.load(fathomId, fathomConfig)
 
-      function onRouteChangeComplete() {
-        Fathom.trackPageview()
-      }
+  //     function onRouteChangeComplete() {
+  //       Fathom.trackPageview()
+  //     }
 
-      router.events.on('routeChangeComplete', onRouteChangeComplete)
+  //     router.events.on('routeChangeComplete', onRouteChangeComplete)
 
-      return () => {
-        router.events.off('routeChangeComplete', onRouteChangeComplete)
-      }
-    }
-  }, [])
+  //     return () => {
+  //       router.events.off('routeChangeComplete', onRouteChangeComplete)
+  //     }
+  //   }
+  // }, [])
 
   return <Component {...pageProps} />
 }
