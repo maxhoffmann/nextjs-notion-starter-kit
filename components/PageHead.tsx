d0fca176cb4fc2deb5920234f7fd345845ee44eb
Maximilian Hoffmann <hey@max.hn>
2022-01-09 16:43:52 +0100

    remove favicons and theme

--- components/PageHead.tsx
+++ components/PageHead.tsx
@@ -1,28 +1,23 @@
-import Head from 'next/head'
-import * as React from 'react'
-import * as types from 'lib/types'
-
-// TODO: remove duplication between PageHead and NotionPage Head
+import Head from "next/head";
+import * as React from "react";
+import * as types from "lib/types";
 
 export const PageHead: React.FC<types.PageProps> = ({ site }) => {
   return (
     <Head>
-      <meta charSet='utf-8' />
-      <meta httpEquiv='Content-Type' content='text/html; charset=utf-8' />
-      <meta
-        name='viewport'
-        content='width=device-width, initial-scale=1, shrink-to-fit=no'
-      />
+      <meta charSet="utf-8" />
+      <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
 
       {site?.description && (
         <>
-          <meta name='description' content={site.description} />
-          <meta property='og:description' content={site.description} />
+          <meta name="description" content={site.description} />
+          <meta property="og:description" content={site.description} />
         </>
       )}
 
-      <meta name='theme-color' content='#EB625A' />
-      <meta property='og:type' content='website' />
+      <meta name="theme-color" content="#ffffff" />
+      <meta property="og:type" content="website" />
     </Head>
-  )
-}
+  );
+};
