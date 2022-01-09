c680af90979a87f6e83b0eac92191a1097334587
Maximilian Hoffmann <hey@max.hn>
2022-01-09 15:00:03 +0100

    remove search

--- components/NotionPage.tsx
+++ components/NotionPage.tsx
@@ -1,40 +1,44 @@
-import * as React from 'react'
-import Head from 'next/head'
-import Link from 'next/link'
-import dynamic from 'next/dynamic'
-import cs from 'classnames'
-import { useRouter } from 'next/router'
-import { useSearchParam } from 'react-use'
-import BodyClassName from 'react-body-classname'
-import { PageBlock } from 'notion-types'
+import * as React from "react";
+import Head from "next/head";
+import Link from "next/link";
+import dynamic from "next/dynamic";
+import cs from "classnames";
+import { useRouter } from "next/router";
+import { useSearchParam } from "react-use";
+import BodyClassName from "react-body-classname";
+import { PageBlock } from "notion-types";
 
-import { Tweet, TwitterContextProvider } from 'react-static-tweets'
+import { Tweet, TwitterContextProvider } from "react-static-tweets";
 
 // core notion renderer
-import { NotionRenderer, Code, Collection, CollectionRow } from 'react-notion-x'
+import {
+  NotionRenderer,
+  Code,
+  Collection,
+  CollectionRow,
+} from "react-notion-x";
 
 // utils
-import { getBlockTitle } from 'notion-utils'
-import { mapPageUrl, getCanonicalPageUrl } from 'lib/map-page-url'
-import { mapNotionImageUrl } from 'lib/map-image-url'
-import { getPageDescription } from 'lib/get-page-description'
-import { getPageTweet } from 'lib/get-page-tweet'
-import { searchNotion } from 'lib/search-notion'
-import * as types from 'lib/types'
-import * as config from 'lib/config'
+import { getBlockTitle } from "notion-utils";
+import { mapPageUrl, getCanonicalPageUrl } from "lib/map-page-url";
+import { mapNotionImageUrl } from "lib/map-image-url";
+import { getPageDescription } from "lib/get-page-description";
+import { getPageTweet } from "lib/get-page-tweet";
+import * as types from "lib/types";
+import * as config from "lib/config";
 
 // components
-import { CustomFont } from './CustomFont'
-import { Loading } from './Loading'
-import { Page404 } from './Page404'
-import { PageHead } from './PageHead'
-import { PageActions } from './PageActions'
-import { Footer } from './Footer'
-import { PageSocial } from './PageSocial'
-import { GitHubShareButton } from './GitHubShareButton'
-import { ReactUtterances } from './ReactUtterances'
+import { CustomFont } from "./CustomFont";
+import { Loading } from "./Loading";
+import { Page404 } from "./Page404";
+import { PageHead } from "./PageHead";
+import { PageActions } from "./PageActions";
+import { Footer } from "./Footer";
+import { PageSocial } from "./PageSocial";
+import { GitHubShareButton } from "./GitHubShareButton";
+import { ReactUtterances } from "./ReactUtterances";
 
-import styles from './styles.module.css'
+import styles from "./styles.module.css";
 
 // const Code = dynamic(() =>
 //   import('react-notion-x').then((notion) => notion.Code)
@@ -59,94 +63,94 @@ import styles from './styles.module.css'
 // )
 
 const Equation = dynamic(() =>
-  import('react-notion-x').then((notion) => notion.Equation)
-)
+  import("react-notion-x").then((notion) => notion.Equation)
+);
 
 // we're now using a much lighter-weight tweet renderer react-static-tweets
 // instead of the official iframe-based embed widget from twitter
 // const Tweet = dynamic(() => import('react-tweet-embed'))
 
 const Modal = dynamic(
-  () => import('react-notion-x').then((notion) => notion.Modal),
+  () => import("react-notion-x").then((notion) => notion.Modal),
   { ssr: false }
-)
+);
 
 export const NotionPage: React.FC<types.PageProps> = ({
   site,
   recordMap,
   error,
-  pageId
+  pageId,
 }) => {
-  const router = useRouter()
-  const lite = useSearchParam('lite')
+  const router = useRouter();
+  const lite = useSearchParam("lite");
 
-  const params: any = {}
-  if (lite) params.lite = lite
+  const params: any = {};
+  if (lite) params.lite = lite;
 
   // lite mode is for oembed
-  const isLiteMode = lite === 'true'
-  const searchParams = new URLSearchParams(params)
+  const isLiteMode = lite === "true";
+  const searchParams = new URLSearchParams(params);
 
   if (router.isFallback) {
-    return <Loading />
+    return <Loading />;
   }
 
-  const keys = Object.keys(recordMap?.block || {})
-  const block = recordMap?.block?.[keys[0]]?.value
+  const keys = Object.keys(recordMap?.block || {});
+  const block = recordMap?.block?.[keys[0]]?.value;
 
   if (error || !site || !keys.length || !block) {
-    return <Page404 site={site} pageId={pageId} error={error} />
+    return <Page404 site={site} pageId={pageId} error={error} />;
   }
 
-  const title = getBlockTitle(block, recordMap) || site.name
+  const title = getBlockTitle(block, recordMap) || site.name;
 
-  console.log('notion page', {
+  console.log("notion page", {
     isDev: config.isDev,
     title,
     pageId,
     rootNotionPageId: site.rootNotionPageId,
-    recordMap
-  })
+    recordMap,
+  });
 
   if (!config.isServer) {
     // add important objects to the window global for easy debugging
-    const g = window as any
-    g.pageId = pageId
-    g.recordMap = recordMap
-    g.block = block
+    const g = window as any;
+    g.pageId = pageId;
+    g.recordMap = recordMap;
+    g.block = block;
   }
 
-  const siteMapPageUrl = mapPageUrl(site, recordMap, searchParams)
+  const siteMapPageUrl = mapPageUrl(site, recordMap, searchParams);
 
   const canonicalPageUrl =
-    !config.isDev && getCanonicalPageUrl(site, recordMap)(pageId)
+    !config.isDev && getCanonicalPageUrl(site, recordMap)(pageId);
 
   // const isRootPage =
   //   parsePageId(block.id) === parsePageId(site.rootNotionPageId)
   const isBlogPost =
-    block.type === 'page' && block.parent_table === 'collection'
-  const showTableOfContents = !!isBlogPost
-  const minTableOfContentsItems = 3
+    block.type === "page" && block.parent_table === "collection";
+  const showTableOfContents = !!isBlogPost;
+  const minTableOfContentsItems = 3;
 
   const socialImage = mapNotionImageUrl(
     (block as PageBlock).format?.page_cover || config.defaultPageCover,
     block
-  )
+  );
 
   const socialDescription =
-    getPageDescription(block, recordMap) ?? config.description
+    getPageDescription(block, recordMap) ?? config.description;
 
-  let comments: React.ReactNode = null
-  let pageAside: React.ReactChild = null
+  let comments: React.ReactNode = null;
+  let pageAside: React.ReactChild = null;
 
   // only display comments and page actions on blog post pages
   if (isBlogPost) {
-    const tweet = getPageTweet(block, recordMap)
+    const tweet = getPageTweet(block, recordMap);
     if (tweet) {
-      pageAside = <PageActions tweet={tweet} />
+      pageAside = <PageActions tweet={tweet} />;
     }
   } else {
-    pageAside = <PageSocial />
+    pageAside = <PageSocial />;
   }
 
   return (
@@ -155,46 +159,46 @@ export const NotionPage: React.FC<types.PageProps> = ({
         tweetAstMap: (recordMap as any).tweetAstMap || {},
         swrOptions: {
           fetcher: (id) =>
-            fetch(`/api/get-tweet-ast/${id}`).then((r) => r.json())
-        }
+            fetch(`/api/get-tweet-ast/${id}`).then((r) => r.json()),
+        },
       }}
     >
       <PageHead site={site} />
 
       <Head>
-        <meta property='og:title' content={title} />
-        <meta property='og:site_name' content={site.name} />
+        <meta property="og:title" content={title} />
+        <meta property="og:site_name" content={site.name} />
 
-        <meta name='twitter:title' content={title} />
-        <meta property='twitter:domain' content={site.domain} />
+        <meta name="twitter:title" content={title} />
+        <meta property="twitter:domain" content={site.domain} />
 
         {config.twitter && (
-          <meta name='twitter:creator' content={`@${config.twitter}`} />
+          <meta name="twitter:creator" content={`@${config.twitter}`} />
         )}
 
         {socialDescription && (
           <>
-            <meta name='description' content={socialDescription} />
-            <meta property='og:description' content={socialDescription} />
-            <meta name='twitter:description' content={socialDescription} />
+            <meta name="description" content={socialDescription} />
+            <meta property="og:description" content={socialDescription} />
+            <meta name="twitter:description" content={socialDescription} />
           </>
         )}
 
         {socialImage ? (
           <>
-            <meta name='twitter:card' content='summary_large_image' />
-            <meta name='twitter:image' content={socialImage} />
-            <meta property='og:image' content={socialImage} />
+            <meta name="twitter:card" content="summary_large_image" />
+            <meta name="twitter:image" content={socialImage} />
+            <meta property="og:image" content={socialImage} />
           </>
         ) : (
-          <meta name='twitter:card' content='summary' />
+          <meta name="twitter:card" content="summary" />
         )}
 
         {canonicalPageUrl && (
           <>
-            <link rel='canonical' href={canonicalPageUrl} />
-            <meta property='og:url' content={canonicalPageUrl} />
-            <meta property='twitter:url' content={canonicalPageUrl} />
+            <link rel="canonical" href={canonicalPageUrl} />
+            <meta property="og:url" content={canonicalPageUrl} />
+            <meta property="twitter:url" content={canonicalPageUrl} />
           </>
         )}
 
@@ -203,12 +207,12 @@ export const NotionPage: React.FC<types.PageProps> = ({
 
       <CustomFont site={site} />
 
-      {isLiteMode && <BodyClassName className='notion-lite' />}
+      {isLiteMode && <BodyClassName className="notion-lite" />}
 
       <NotionRenderer
         bodyClassName={cs(
           styles.notion,
-          pageId === site.rootNotionPageId && 'index-page'
+          pageId === site.rootNotionPageId && "index-page"
         )}
         components={{
           pageLink: ({
@@ -240,7 +244,7 @@ export const NotionPage: React.FC<types.PageProps> = ({
           collectionRow: CollectionRow,
           tweet: Tweet,
           modal: Modal,
-          equation: Equation
+          equation: Equation,
         }}
         recordMap={recordMap}
         rootPageId={site.rootNotionPageId}
@@ -254,7 +258,6 @@ export const NotionPage: React.FC<types.PageProps> = ({
         defaultPageCoverPosition={config.defaultPageCoverPosition}
         mapPageUrl={siteMapPageUrl}
         mapImageUrl={mapNotionImageUrl}
-        searchNotion={searchNotion}
         pageFooter={comments}
         pageAside={pageAside}
         footer={<Footer />}
@@ -262,5 +265,5 @@ export const NotionPage: React.FC<types.PageProps> = ({
 
       <GitHubShareButton />
     </TwitterContextProvider>
-  )
-}
+  );
+};
